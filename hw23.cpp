de61fbe8d8070725317bb2d9f6337037207cb2bd
mrj <m@badcrumble.net>
2024-07-28 18:37:11 +0100

    Add content to README, and trivial cpp.

---
+++ hw23.cpp
@@ -0,0 +1,41 @@
+// ACCU Homework Challenge 23
+//
+// From https://accu.org/journals/cvu/cvu36-3
+//
+// Write a program that will evaluate a hand when playing 6-card Cribbage. 
+//
+// There will be five cards: 4 in the hand and the starter. 
+// The distinction between hand and starter only matters for flushes. 
+// Here is a brief summary of the scoring:
+// 
+// Fifteen. Each combination of cards that totals 15 scores 2 (picture cards count as 10 and the ace – always low – counts as 1).
+// Pair.    Each pair of cards of the same rank scores 2 (so a set of 3 scores 6 and a set of four scores 12 – there are 6 ways of selecting two from 4).
+// Run.     Each combination of three or more cards in sequence scores 1 for each card in the sequence.
+// Flush.   Four cards of the same suit in hand (excluding the starter) scores 4.
+//          Four cards in hand the same suit as the starter scores 5
+// His Nobs. Jack of the same suit as starter scores 1 (no score if the starter is a Jack).
+// 
+// You score for each and every combination of two cards that make a pair, of two or more cards that make 15, or of three or more cards that make a run. Note that a run of 4 just counts 4 even though it includes 2 runs of 3.
+// Example: ♠6, ♠5, ♥4, ♦4, ♣5.
+// Four ways of making 15 scores 8 (♠6 ♠5 ♥4; ♠6 ♠5 ♦4; ♠6 ♣5 ♥4; ♠6, ♣5, ♦4).
+// Four runs of 6 5 4 score 12 (♠6 ♠5 ♥4; ♠6 ♠5 ♦4; ♠6 ♣5 ♥4; ♠6 ♣5 ♦4).
+// Two pairs score 4 (♥4, ♦4 and ♠5, ♣5).
+// Total 24.
+
+#include <iostream>
+
+using namespace std;
+
+
+void test(const string& a, const char* b)
+{
+    if (a == string{b})
+        cout << "Test passed\n";
+    else
+        cerr << "Test FAILED! : \"" << a << "\" != \"" << b << "\"\n";
+}
+
+int main()
+{
+    return 0;
+}
